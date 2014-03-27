38f946648b4cba5280cd3eba86df18d920018d5c
SDionis <SDionis@yandex.ru>
2014-03-28 00:06:48 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UkraineTweets::Application.config.secret_key_base = '60b677bbf20296d33ca462d7441b3873c93351c921816d71c05b995b8b4c8e23b01d8adb765afa78ddb9138d9c5efd4c02787c41c6bbdf774e66e562b86c0fac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UkraineTweets::Application.config.session_store :cookie_store, key: '_ukraine_tweets_session'
